acef9c7b46068b2ff2a7875395b744e6c72eec06
Kartik Neti <kartikprajak@gmail.com>
2023-10-23 16:44:45 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import requests
+
+api_key = '54NWkU17GwLzxNUfs0cRaADljkyt3ySBBmPiiyV5'
+url = f'https://api.nasa.gov/planetary/apod?api_key={api_key}'
+
+response = requests.get(url)
+data = response.json()
+
+title = data["title"]
+image_url = data['hdurl']
+date = data["date"]
+content = data['explanation']
+
+print(data)
+
+
+
+st.title(title)
+st.image(image_url)
+st.subheader(date)
+st.write(content)
